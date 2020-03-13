25bb1dc084b82987f3c014450bec2493adba3440
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-13 16:44:23 +0200

    Revert "Change project name"
    This reverts commit 071ffc0d2652d8d7c4772ec7c7dd37853b821375.

--- Domain/Identity/AppUserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Domain
-{
-    public class AppUserRole : IdentityRole
-    {
-        public override string Id { get; set; } = default!;
-    }
-}
