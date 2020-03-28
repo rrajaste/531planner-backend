660bd6088e885fd0f5d2ba18379fe88ac1b337b0
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-28 23:02:48 +0200

    Add all repositories to interface

--- Contracts.DAL.App/IAppUnitOfWork.cs
+++ Contracts.DAL.App/IAppUnitOfWork.cs
@@ -1,11 +1,26 @@
+using Contracts.DAL.App.Repositories;
 using Contracts.DAL.Base;
-using Domain;
+
 
 namespace Contracts.DAL.App
 {
     public interface IAppUnitOfWork : IBaseUnitOfWork
     {
         IBodyMeasurementRepository BodyMeasurements { get; }
-        IUnitsTypeRepository UnitTypes { get; }
+        IDailyNutritionIntakeRepository DailyNutritionIntakes { get; }
+        IExerciseInTrainingDayRepository ExercisesInTrainingDays { get; }
+        IExerciseRepository Exercises { get; }
+        IExerciseTypeRepository ExerciseTypes { get; }
+        IMuscleGroupRepository MuscleGroups { get; }
+        IMuscleRepository Muscles { get; }
+        IPerformedExerciseRepository PerformedExercises { get; }
+        IRoutineTypeRepository RoutineTypes { get; }
+        ITargetMuscleGroupRepository TargetMuscleGroups { get; }
+        ITrainingCycleRepository TrainingCycles { get; }
+        ITrainingDayRepository TrainingDays { get; }
+        ITrainingDayTypeRepository TrainingDayTypes { get; }
+        ITrainingWeekRepository TrainingWeeks { get; }
+        IUnitTypesRepository UnitTypes { get; }
+        IWorkoutRoutineRepository WorkoutRoutines { get; }
     }
 }
