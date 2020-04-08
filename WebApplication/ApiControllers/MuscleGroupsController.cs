404c7122f5c17d4d076c408780214878ce2ff181
RannoRajaste <ranno.rajaste@gmail.com>
2020-04-08 20:20:12 +0300

    Add new REST api controller

---
+++ WebApplication/ApiControllers/MuscleGroupsController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts.DAL.App;
+using Microsoft.AspNetCore.Mvc;
+using Domain;
+using PublicApi.DTO.V1.MuscleGroups;
+
+namespace WebApplication.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MuscleGroupsController : ControllerBase
+    {
+        private readonly IAppUnitOfWork _unitOfWork;
+
+        public MuscleGroupsController(IAppUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/MuscleGroups
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MuscleGroupDetailsDto>>> GetMuscleGroups()
+        {
+            var muscleGroups = await _unitOfWork.MuscleGroups.AllAsync();
+            return Ok(muscleGroups.Select(CreateNewDtoFromDomainEntity));
+        }
+
+        // GET: api/MuscleGroups/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MuscleGroupDetailsDto>> GetMuscleGroup(Guid id)
+        {
+            var muscleGroup = await _unitOfWork.MuscleGroups.FindAsync(id);
+            if (muscleGroup == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(CreateNewDtoFromDomainEntity(muscleGroup));
+        }
+
+        // PUT: api/MuscleGroups/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMuscleGroup(Guid id, MuscleGroupEditDto dto)
+        {
+            if (id != Guid.Parse(dto.Id))
+            {
+                return BadRequest();
+            }
+            
+            var muscleGroup = await _unitOfWork.MuscleGroups.FindAsync(id);
+            if (muscleGroup == null)
+            {
+                return NotFound();
+            }
+            MapDtoToDomainEntity(dto, muscleGroup);
+            await _unitOfWork.SaveChangesAsync();
+            return NoContent();
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<MuscleGroup>> PostMuscleGroup(MuscleGroup muscleGroup)
+        {
+            _unitOfWork.MuscleGroups.Add(muscleGroup);
+            await _unitOfWork.SaveChangesAsync();
+
+            return CreatedAtAction("GetMuscleGroup", new { id = muscleGroup.Id }, muscleGroup);
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<MuscleGroup>> DeleteMuscleGroup(Guid id)
+        {
+            var muscleGroup = await _unitOfWork.MuscleGroups.FindAsync(id);
+            if (muscleGroup == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.MuscleGroups.Remove(muscleGroup);
+            await _unitOfWork.SaveChangesAsync();
+
+            return muscleGroup;
+        }
+        
+        private static MuscleGroupDetailsDto CreateNewDtoFromDomainEntity(MuscleGroup muscleGroup)
+        {
+            return new MuscleGroupDetailsDto()
+            {
+                Id = muscleGroup.Id.ToString(),
+                Name = muscleGroup.Name,
+                Description = muscleGroup.Description,
+                CreatedAt = muscleGroup.CreatedAt.ToString(CultureInfo.InvariantCulture),
+            };
+        }
+            
+        private static void MapDtoToDomainEntity<TDto>(TDto dto, MuscleGroup muscleGroup) 
+            where TDto : MuscleGroupCreateDto
+        {
+            muscleGroup.Name = dto.Name;
+            muscleGroup.Description = dto.Description;
+        }
+    }
+}
---
+++ WebApplication/ApiControllers/MusclesController.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts.DAL.App;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DAL.App.EF;
+using Domain;
+using PublicApi.DTO.V1.BaseDTOs.BaseDictionaryTypeDto;
+using PublicApi.DTO.V1.Muscle;
+using PublicApi.DTO.V1.MuscleGroups;
+
+namespace WebApplication.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusclesController : ControllerBase
+    {
+        private readonly IAppUnitOfWork _unitOfWork;
+
+        public MusclesController(IAppUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MuscleDetailsDto>>> GetMuscles()
+        {
+            var muscles = await _unitOfWork.Muscles.AllAsync();
+            return Ok(muscles.Select(CreateNewDtoFromDomainEntity));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MuscleDetailsDto>> GetMuscle(Guid id)
+        {
+            var muscle = await _unitOfWork.Muscles.FindAsync(id);
+
+            if (muscle == null)
+            {
+                return NotFound();
+            }
+            return Ok(CreateNewDtoFromDomainEntity(muscle));
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMuscle(Guid id, MuscleEditDto dto)
+        {
+            if (id != Guid.Parse(dto.Id))
+            {
+                return BadRequest();
+            }
+            var muscle = await _unitOfWork.Muscles.FindAsync(id);
+            if (muscle == null)
+            {
+                return NotFound();
+            }
+            MapDtoToDomainEntity(dto, muscle);
+            await _unitOfWork.SaveChangesAsync();
+            return NoContent();
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Muscle>> PostMuscle(MuscleCreateDto dto)
+        {
+            var muscle = new Muscle();
+            MapDtoToDomainEntity(dto, muscle);
+            _unitOfWork.Muscles.Add(muscle);
+            await _unitOfWork.SaveChangesAsync();
+
+            return CreatedAtAction("GetMuscle", new { id = muscle.Id }, muscle);
+        }
+
+        // DELETE: api/Muscles/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Muscle>> DeleteMuscle(Guid id)
+        {
+            var muscle = await _unitOfWork.Muscles.FindAsync(id);
+            if (muscle == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Muscles.Remove(muscle);
+            await _unitOfWork.SaveChangesAsync();
+            return Ok();
+        }
+        private static MuscleDetailsDto CreateNewDtoFromDomainEntity(Muscle muscle)
+            {
+                return new MuscleDetailsDto()
+                {
+                    Id = muscle.Id.ToString(),
+                    Name = muscle.Name,
+                    Description = muscle.Description,
+                    CreatedAt = muscle.CreatedAt.ToString(CultureInfo.InvariantCulture),
+                    MuscleGroup = new MuscleGroupDto()
+                    {
+                        Name = muscle.MuscleGroup.Name,
+                        Description = muscle.MuscleGroup.Description
+                    }
+                };
+            }
+            
+        private static void MapDtoToDomainEntity<TDto>(TDto dto, Muscle muscle) 
+                where TDto : MuscleCreateDto
+        {
+            muscle.Name = dto.Name;
+            muscle.Description = dto.Description;
+            muscle.MuscleGroupId = Guid.Parse(dto.MuscleGroupId);
+        }
+    }
+}
