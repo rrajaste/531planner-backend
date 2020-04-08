11df25552a2419b474faef8b0440de419378944c
RannoRajaste <ranno.rajaste@gmail.com>
2020-04-08 20:18:14 +0300

    Add base DTOs for dictionary type entities

---
+++ PublicApi.DTO.V1/BaseDTOs/BaseDictionaryTypeDto/BaseCreateDto.cs
@@ -0,0 +1,8 @@
+namespace PublicApi.DTO.V1.BaseDTOs.BaseDictionaryTypeDto
+{
+    public class BaseCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PublicApi.DTO.V1/BaseDTOs/BaseDictionaryTypeDto/BaseDto.cs
@@ -0,0 +1,9 @@
+namespace PublicApi.DTO.V1.BaseDTOs.BaseDictionaryTypeDto
+{
+    public class BaseDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
