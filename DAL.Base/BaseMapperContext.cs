28fb42fa7df418c7f7742dee064d7048547f5c30
Ranno Rajaste <raraja@ttu.ee>
2020-04-23 21:32:02 +0300

    Remove unnecessary BaseDALMapper

---
+++ DAL.Base/BaseMapperContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace DAL.Base
+{
+    public class BaseMapperContext
+    {
+        private readonly Dictionary<Type, object> _mapperCache = new Dictionary<Type, object>();
+        
+        public TMapper GetMapper<TMapper>(Func<TMapper> mapperCreationMethod)
+        {
+            if (_mapperCache.TryGetValue(typeof(TMapper), out var mapper))
+            {
+                return (TMapper) mapper;
+            }
+
+            mapper = mapperCreationMethod()!;
+            _mapperCache.Add(typeof(TMapper), mapper);
+            return (TMapper) mapper;
+        }
+    }
+}
--- DAL.Base/Mappers/IBaseDALMapper.cs
+++ DAL.Base/Mappers/IBaseDALMapper.cs
@@ -1,13 +1,10 @@
 namespace DAL.Base.Mappers
 {
-    public interface IBaseDALMapper<in TInObject, out TOutObject> 
+    public interface IBaseDALMapper<TInObject, TOutObject> 
         where TOutObject : class, new()
         where TInObject : class, new()
     {
-        TOutObject Map(TInObject inObject);
-
-        TMapOutObject Map<TMapInObject, TMapOutObject>(TMapInObject inObject)
-            where TMapOutObject : class, new()
-            where TMapInObject : class;
+        TOutObject MapDomainToDAL(TInObject domainObject);
+        TInObject MapDALToDomain(TOutObject dalObject);
     }
 }
